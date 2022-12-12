7224db1d7204a5aad61511f163ba72f941c41819
xtopala <xtopala@gmail.com>
2022-12-12 08:11:37 +0100

    finished heat map

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,8 +1,18 @@
 using CodeMonkey.Utils;
+using System;
 using UnityEngine;
 
 public class Grid
 {
+    public const int HEAT_MAP_MAX_VALUE = 100;
+    public const int HEAT_MAP_MIN_VALUE = 0;
+    public event EventHandler<OnGridValueChangedEventArgs> OnGridValueChanged;
+    public class OnGridValueChangedEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
+
     private int width;
     private int height;
     private float cellSize;
@@ -18,19 +28,49 @@ public class Grid
         this.originPosition = originPosition;
 
         gridArray = new int[width, height];
-        debugTextArray = new TextMesh[width, height];
 
-        for (int x=0; x < gridArray.GetLength(0); x++)
+        bool showDebug = false;
+        if (showDebug)
         {
-            for (int y=0; y < gridArray.GetLength(1); y++)
+            TextMesh[,] debugTextArray = new TextMesh[width, height];
+
+            for (int x = 0; x < gridArray.GetLength(0); x++)
             {
-                debugTextArray[x, y] = UtilsClass.CreateWorldText(gridArray[x,y].ToString(), null, GetWordlPosition(x,y) + new Vector3(cellSize,cellSize) * .5f, 20, Color.white, TextAnchor.MiddleCenter);
-                Debug.DrawLine(GetWordlPosition(x, y), GetWordlPosition(x, y + 1), Color.white, 100f);
-                Debug.DrawLine(GetWordlPosition(x, y), GetWordlPosition(x + 1, y), Color.white, 100f);
+                for (int y = 0; y < gridArray.GetLength(1); y++)
+                {
+                    debugTextArray[x, y] = UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWordlPosition(x, y) + new Vector3(cellSize, cellSize) * .5f, 20, Color.white, TextAnchor.MiddleCenter);
+                    Debug.DrawLine(GetWordlPosition(x, y), GetWordlPosition(x, y + 1), Color.white, 100f);
+                    Debug.DrawLine(GetWordlPosition(x, y), GetWordlPosition(x + 1, y), Color.white, 100f);
+                }
+                Debug.DrawLine(GetWordlPosition(0, height), GetWordlPosition(width, height), Color.white, 100f);
+                Debug.DrawLine(GetWordlPosition(width, 0), GetWordlPosition(width, height), Color.white, 100f);
+
+                OnGridValueChanged += (object sender, OnGridValueChangedEventArgs eventArgs) =>
+                {
+                    debugTextArray[eventArgs.x, eventArgs.y].text = gridArray[eventArgs.x, eventArgs.y].ToString();
+                };
             }
-            Debug.DrawLine(GetWordlPosition(0, height), GetWordlPosition(width, height), Color.white, 100f);
-            Debug.DrawLine(GetWordlPosition(width,0), GetWordlPosition(width, height), Color.white, 100f);
         }
+    }
+
+    public int GetWidth()
+    {
+        return width;
+    }
+
+    public int GetHeight()
+    {
+        return height;
+    }
+
+    public float GetCellSize()
+    {
+        return cellSize;
+    }
+
+    public Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize + originPosition;
     }
 
     public int GetValue(int x, int y)
@@ -38,7 +78,8 @@ public class Grid
         if (x >= 0 && y >= 0 && x < width && y < height)
         {
             return gridArray[x, y];
-        } else
+        }
+        else
         {
             return 0;
         }
@@ -55,8 +96,9 @@ public class Grid
     {
         if (x >= 0 && y >= 0 && x < width && y < height)
         {
-            gridArray[x, y] = value;
-            debugTextArray[x, y].text = gridArray[x, y].ToString();
+            gridArray[x, y] = Mathf.Clamp(value, HEAT_MAP_MIN_VALUE, HEAT_MAP_MAX_VALUE);
+            //  if (OnGridValueChanged != null) OnGridValueChanged(this, new OnGridValueChangedEventArgs { x = x, y = y });
+            OnGridValueChanged?.Invoke(this, new OnGridValueChangedEventArgs { x = x, y = y }); // <- shorthand
         }
     }
 
@@ -67,14 +109,52 @@ public class Grid
         SetValue(x, y, value);
     }
 
+    public void AddValue(int x, int y, int value)
+    {
+        SetValue(x, y, GetValue(x, y) + value);
+    }
+
+    public void AddValue(Vector3 worldPosition, int value, int fullValueRange, int totalRange)
+    {
+        int lowerValueAmount = Mathf.RoundToInt((float)value / (totalRange - fullValueRange));
+
+        GetXY(worldPosition, out int originX, out int originY);
+        for (int x = 0; x < totalRange; x++)
+        {
+            for (int y = 0; y < totalRange - x; y++)
+            {
+                int radius = x + y;
+                int addValueAmount = value;
+                if (radius > fullValueRange)
+                {
+                    addValueAmount -= lowerValueAmount * (radius - fullValueRange);
+                }
+
+                AddValue(originX + x, originY + y, addValueAmount);
+                if (x != 0)
+                {
+                    AddValue(originX - x, originY + y, addValueAmount);
+                }
+                if (y != 0)
+                {
+                    AddValue(originX + x, originY - y, addValueAmount);
+                    if (x != 0)
+                    {
+                        AddValue(originX - x, originY - y, addValueAmount);
+                    }
+                }
+            }
+        }
+    }
+
     private Vector3 GetWordlPosition(int x, int y)
     {
         return new Vector3(x, y) * cellSize + originPosition;
     }
-    
+
     private void GetXY(Vector3 worldPosition, out int x, out int y)
     {
-        x = Mathf.FloorToInt((worldPosition-originPosition).x / cellSize);
-        y = Mathf.FloorToInt((worldPosition-originPosition).y / cellSize);
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
     }
 }
--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    [SerializeField] private HeatMapVisual heatMapVisual;
     private Grid grid;
 
     // Start is called before the first frame update
     void Start()
     {
-        grid = new Grid(4, 2, 10f, new Vector3(20, 0));
+        grid = new Grid(100, 100, 4f, Vector3.zero);
+
+        heatMapVisual.SetGrid(grid);
     }
 
     // Update is called once per frame
@@ -18,7 +21,8 @@ public class Testing : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+            Vector3 position = UtilsClass.GetMouseWorldPosition();
+            grid.AddValue(position, 100, 5, 40);
         }
 
         if (Input.GetMouseButtonDown(1))
